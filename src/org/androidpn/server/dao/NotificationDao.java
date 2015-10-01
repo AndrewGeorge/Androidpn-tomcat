03dca617fcda1b6e21bf77bcd5fbc6e32cce1aab
george <shaodf2012@gmail.com>
2015-10-01 19:38:46 +0800

    完成服务端离线消息的发送和优化

---
+++ src/org/androidpn/server/dao/NotificationDao.java
@@ -0,0 +1,16 @@
+package org.androidpn.server.dao;
+
+import java.util.List;
+
+import org.androidpn.server.model.Notification;
+
+
+public interface NotificationDao {
+
+	void saveNotification(Notification notification);
+
+	List<Notification> findNotificationsByUsername(String username);
+
+	void deleteNotification(Notification notification);
+	void deleteNotificationByUUid(String  uuid);
+}
